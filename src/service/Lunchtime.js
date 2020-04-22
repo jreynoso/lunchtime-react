e86fd1de763b06992631dc73aa7b99733a6abae0
jreynoso <jreynoso@gmail.com>
2020-04-22 10:22:44 -0500

    set production urls and env configurable lunchtime api url

--- src/service/Lunchtime.js
+++ src/service/Lunchtime.js
@@ -1,5 +1,13 @@
+import config from '../config/config.json'
+const env = process.env.NODE_ENV || 'development'
+
 export default function fetchLunchOptions (loc, mode) {
-  return window.fetch(`http://localhost:8080/lunchtime?loc=${loc}&mode=${mode}`)
+  const envConfig = config[env]
+  if (!envConfig || !envConfig.lunchtime_api_url) {
+    console.error(`lunchtime_api_url is not configured for ${env}`)
+    return
+  }
+  return window.fetch(`${envConfig.lunchtime_api_url}/lunchtime?loc=${loc}&mode=${mode}`)
     .then(r => r.json())
     .then(response => response || 'empty')
 }
